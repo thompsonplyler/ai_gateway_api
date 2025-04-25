5df768764b541b4a56c89fca486cf70a7dfecbfc
Thompson Plyler <nealok.g@gmail.com>
2025-04-25 13:10:57 -0400

    Add root health check.

--- config/routes.rb
+++ config/routes.rb
@@ -26,5 +26,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  # Point root to the rails health check endpoint
+  root "rails/health#show"
 end
